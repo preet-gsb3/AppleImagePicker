2b65055e4fc09423712b609419c85dfc88f28d1e
Gurpreet Singh <preet.gsb3@gmail.com>
2020-02-11 23:55:15 +0530

    Initial Commit

---
+++ DG-ImagePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DG-ImagePicker
+//
+//  Created by Gurpreet Singh on 11/02/20.
+//  Copyright © 2020 Gurpreet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
